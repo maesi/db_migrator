dda14c5356f878d81b850d56818f0f279d350065
maesi <mail@maesi.org>
2015-04-01 16:48:34 +0200

    add Log Level class

---
+++ logger/Level.php
@@ -0,0 +1,34 @@
+<?php
+namespace logger;
+
+final class Level {
+	
+	static function debug() {
+		return new Level(1, "debug");
+	}
+	
+	static function info() {
+		return new Level(2, "info");
+	}
+	
+	static function warning() {
+		return new Level(3, "warning");
+	}
+	
+	static function error() {
+		return new Level(4, "error");
+	}
+	
+	private $level;
+	private $message;
+	
+	private function __construct($level, $message) {
+		$this->level = $level;
+		$this->message = $message;
+	}
+	
+	function __toString() {
+		return $this->message;
+	}
+	
+}
