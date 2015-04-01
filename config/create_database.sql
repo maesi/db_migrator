f2a09469b8db86788c94c2de8f447d3e8394733a
maesi <mail@maesi.org>
2015-04-01 16:19:30 +0200

    add config sqls

---
+++ config/create_database.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE IF NOT EXISTS dbmigrator;
+USE dbmigrator;
---
+++ config/create_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS _migration (
+	id int(4) unsigned NOT NULL AUTO_INCREMENT,
+	version varchar(10) NOT NULL,
+	name varchar(255) DEFAULT NULL,
+	checksum varchar(32) NOT NULL,	
+	PRIMARY KEY(id),
+	UNIQUE KEY(version)
+);
