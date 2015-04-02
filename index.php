e67ff55da2748e4ecbb9ea654b207211cc30fb0a
maesi <mail@maesi.org>
2015-04-02 13:15:47 +0200

    remove unused imports

--- index.php
+++ index.php
@@ -4,14 +4,11 @@ function __autoload($name) {
 	$include = "class/". str_replace("\\", "/", $name) . ".php";
 	include ($include);
 }
-use dbmigrator\ExecutableCreator;
-use dbmigrator\MySQL;
-use dbmigrator\DBFactory;
+use dbmigrator\DBMigrator;
 use logger\ConsoleWriter;
 use logger\FileWriter;
 use logger\Logger;
 use logger\Level;
-use dbmigrator\DBMigrator;
 
 Logger::addWriter(new ConsoleWriter(Level::info()));
 Logger::addWriter(new FileWriter(Level::warning(), "log.txt"));
