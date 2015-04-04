f5c77297785f02c1b5527e3652fc0d18211bb514
Marcel Arnold <mail@maesi.org>
2015-04-05 01:25:34 +0200

    merge FileExecutor into SqlFileExecutor

--- class/dbmigrator/SqlFileExecutor.php
+++ class/dbmigrator/SqlFileExecutor.php
@@ -3,11 +3,48 @@ namespace dbmigrator;
 
 use logger\Logger;
 
-class SqlFileExecutor extends FileExecutor {
+class SqlFileExecutor implements Executable {
 
-	function execute(DB $data) {
-		parent::execute($data);
+	private $file;
+	
+	private $content;
+	
+	function __construct($file) {
+		$this->file = $file;
+		$this->readContent();
+	}
+	
+	function execute(DB $database) {
+		$sql = 'INSERT INTO _migration (version, name, checksum)';
+		$sql .= ' VALUES ("' . $this->getVersion() . '", "' . $this->getName() . '", "' . $this->getHash() . '")';
+		$database->execute($sql);
 		Logger::debug($this->getVersion() . " -> " . $this->getName());
-		$data->execute($this->content);
+		$database->execute($this->content);
+	}
+	
+	function getVersion() {
+		$filename = $this->getFilename();
+		return substr($filename, 0, strpos($filename, "_"));
+	}
+	
+	function getName() {
+		$filename = $this->getFilename();
+		return substr($filename, strpos($filename, "_") + 1, strrpos($filename, ".") - strpos($filename, "_") - 1);
+	}
+	
+	private function getFilename() {
+		if(strrpos($this->file, "/")) {
+			return substr($this->file, strrpos($this->file, "/") + 1);
+		} else {
+			return $this->file;
+		}
+	}
+	
+	private function readContent() {
+		$this->content = file_get_contents($this->file);
+	}
+	
+	function getHash() {
+		return sha1_file($this->file);
 	}
 }
