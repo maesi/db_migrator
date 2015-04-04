97ed1242d81f71a32f8e1564740e80d422d60daa
Marcel Arnold <mail@maesi.org>
2015-04-05 01:15:46 +0200

    increase size of checksum

--- config/mysql/create_table.sql
+++ config/mysql/create_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS _migration (
 	id int(4) unsigned NOT NULL AUTO_INCREMENT,
 	version varchar(10) NOT NULL,
 	name varchar(255) DEFAULT NULL,
-	checksum varchar(32) NOT NULL,	
+	checksum varchar(40) NOT NULL,	
 	PRIMARY KEY(id),
 	UNIQUE KEY(version)
 );
