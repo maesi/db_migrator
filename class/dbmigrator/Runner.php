6a8eb9e3f804b517c057dc89e332e55c7a3b7415
maesi <mail@maesi.org>
2015-04-28 15:28:13 +0200

    create Runner class for easy using

---
+++ class/dbmigrator/Runner.php
@@ -0,0 +1,35 @@
+<?php
+namespace dbmigrator;
+
+use logger\WriterFactory;
+use logger\Logger;
+
+class Runner {
+
+	public static function fromIni($inifile = 'dbmigrator.ini') {
+		$config = parse_ini_file('dbmigrator.ini', true);
+		
+		self::fromConfig($config);
+	}
+	
+	public static function fromConfig(array $config) {
+		self::initLogger($config);
+		
+		try {
+			$dbmigrator = new DBMigrator($config['db.connection']);
+			$dbmigrator->migrate($config['src']['directory']);
+		} catch(Exception $ex) {
+			Logger::error($ex);
+		}
+	}
+
+	private static function initLogger(array $config) {
+		if(@$config['logger']['enabled']) {
+			$writers = explode(',', $config['logger']['enabled']);
+			foreach($writers as $writer) {
+				$keyName = "logger.writer." . trim($writer);
+				Logger::addWriter(WriterFactory::create($config[$keyName]));
+			}
+		}
+	}
+}
--- index.php
+++ index.php
@@ -1,24 +1,6 @@
 <?php
 include 'stub.php';
 
-use dbmigrator\DBMigrator;
-use logger\Logger;
-use logger\WriterFactory;
+use dbmigrator\Runner;
 
-$config = parse_ini_file('dbmigrator.ini', true);
-
-try {
-	if(@$config['logger']['enabled']) {
-		$writers = explode(',', $config['logger']['enabled']);
-		foreach($writers as $writer) {
-			$keyName = "logger.writer.".trim($writer);
-			Logger::addWriter(WriterFactory::create($config[$keyName]));
-		}
-	}
-	
-	$dbmigrator = new DBMigrator($config['db.connection']);
-	$dbmigrator->migrate($config['src']['directory']);
-} catch(Exception $ex) {
-	Logger::error($ex);
-}
-?>
+Runner::fromIni();
