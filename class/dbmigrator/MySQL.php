bbf6a6a023681644c8dce9d5f63c7387c524c63e
Marcel Arnold <mail@maesi.org>
2015-05-06 14:15:35 +0200

    execute sql file in a transaction

--- class/dbmigrator/MySQL.php
+++ class/dbmigrator/MySQL.php
@@ -14,6 +14,7 @@ class MySQL implements DB {
 
 	function execute($statement) {
 		$statements = preg_split("/;/", $statement);
+		$this->startTransaction();
 		foreach($statements as $stmt) {
 			$stmt = trim($stmt) . ";";
 			if($stmt == ";") {
@@ -21,12 +22,31 @@ class MySQL implements DB {
 			}
 			Logger::debug($stmt);
 			if(!$result = $this->mysqli->query($stmt)) {
+				$this->rollback();
 				throw new \Exception($this->mysqli->errno . ":" . $this->mysqli->error);
 			}
 			if(@$result->num_rows > 0) {
 				return $result->fetch_object();
 			}
 		}
+		$this->commit();
+	}
+	
+	private function startTransaction() {
+		$this->mysqli->autocommit(FALSE);
+		Logger::debug('startTransaction');
+	}
+	
+	private function commit() {
+		$this->mysqli->commit();
+		$this->mysqli->autocommit(TRUE);		
+		Logger::debug('commit');
+	}
+	
+	private function rollback() {
+		$this->mysqli->rollback();
+		$this->mysqli->autocommit(TRUE);
+		Logger::debug('rollback');
 	}
 }
 
