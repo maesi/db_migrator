c4b7d88eaae6ad3a08021849b327c3ad9e5d138d
Marcel Arnold <mail@maesi.org>
2015-05-26 02:32:59 +0200

    add autoloading for tests

--- tests/DummyTest.php
+++ tests/DummyTest.php
@@ -1,8 +1,11 @@
 <?php
+namespace maesi\dbmigrator\tests;
+
 class DummyTest extends PHPUnit_Framework_TestCase
 {
 	public function testDummy()
 	{
+		Logger::info('d');
 		$this->assertTrue(true);
 	}
 }
