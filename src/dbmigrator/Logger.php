dd07362c4de36f2e70d9f8de6f17f171e30c02d3
Marcel Arnold <mail@maesi.org>
2015-05-26 02:19:27 +0200

    only log message if logger is registered

--- src/dbmigrator/Logger.php
+++ src/dbmigrator/Logger.php
@@ -6,16 +6,26 @@ use Monolog\Registry;
 class Logger {
 
 	public static function debug($message) {
-		Registry::getInstance('dbmigrator')->addDebug($message);
+		if(Registry::hasLogger('dbmigrator')) {
+			Registry::getInstance('dbmigrator')->addDebug($message);
+		}
 	}
+
 	public static function info($message) {
-		Registry::getInstance('dbmigrator')->addInfo($message);
+		if(Registry::hasLogger('dbmigrator')) {
+			Registry::getInstance('dbmigrator')->addInfo($message);
+		}
 	}
+
 	public static function warning($message) {
-		Registry::getInstance('dbmigrator')->addWarning($message);
+		if(Registry::hasLogger('dbmigrator')) {
+			Registry::getInstance('dbmigrator')->addWarning($message);
+		}
 	}
+
 	public static function error($message) {
-		Registry::getInstance('dbmigrator')->addError($message);
+		if(Registry::hasLogger('dbmigrator')) {
+			Registry::getInstance('dbmigrator')->addError($message);
+		}
 	}
-	
 }
