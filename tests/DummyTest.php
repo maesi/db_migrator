7b2e11a41aee35d1e6e334690580a1528a907045
Marcel Arnold <mail@maesi.org>
2015-05-26 02:34:15 +0200

    correct namespace of phpunit class

--- tests/DummyTest.php
+++ tests/DummyTest.php
@@ -1,11 +1,10 @@
 <?php
 namespace maesi\dbmigrator\tests;
 
-class DummyTest extends PHPUnit_Framework_TestCase
+class DummyTest extends \PHPUnit_Framework_TestCase
 {
 	public function testDummy()
 	{
-		Logger::info('d');
 		$this->assertTrue(true);
 	}
 }
